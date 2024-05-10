475535e6f8e37bfaf027ae636cf038ccce0804fb
DmitryAstafyev <dvastafyev@gmail.com>
2024-05-10 17:36:23 +0200

    Correct zooming and label of it

--- lib/src/zoomlabel.ts
+++ lib/src/zoomlabel.ts
@@ -9,17 +9,18 @@ export class ZoomLabel {
         this.label.style.top = "50%";
         this.label.style.left = "50%";
         this.label.style.marginLeft = "-30px";
-        this.label.style.marginTop = "-24px";
+        this.label.style.marginTop = "-11px";
         this.label.style.display = "none";
-        this.label.style.position = "absolute";
+        this.label.style.position = "fixed";
         this.label.style.width = "60px";
-        this.label.style.height = "30px";
         this.label.style.textAlign = "center";
         this.label.style.fontSize = "18px";
-        this.label.style.fontStyle = "bold";
+        this.label.style.lineHeight = "18px";
+        this.label.style.fontWeight = "bold";
         this.label.style.userSelect = "none";
         this.label.style.background = "rgba(255,255,255,0.4)";
-        this.label.style.paddingTop = "16px";
+        this.label.style.padding = "8px 4px 0px 4px";
+        this.label.style.borderRadius = "3px";
         parent.appendChild(this.label);
     }
 
@@ -28,11 +29,11 @@ export class ZoomLabel {
     }
 
     public show(factor: number) {
-        this.label.innerHTML = `${(factor * 100).toFixed(0)}%`;
+        this.label.textContent = `${(factor * 100).toFixed(0)}%`;
         this.label.style.display = "block";
         clearTimeout(this.timer);
         this.timer = setTimeout(() => {
-            this.label.style.display = "none";
+            // this.label.style.display = "none";
         }, HIDE_IN_MS) as unknown as number;
     }
 }
